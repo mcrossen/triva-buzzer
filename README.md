d5418f20f180713c6ca8276601ba06686144133a
Mark Crossen <markcrossen@studentbody.byu.edu>
2018-09-29 14:07:16 -0700

    non-blocking ding when buzzed

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # triva-buzzer
 quck and hacky website to replace trivia buzzers for group games
 
+## Installation
+On Ubuntu, install vlc media player:
+`sudo apt-get install vlc`
+
+Then install the repository dependencies:
+`pip install -r requirements.txt`
+
 ## Usage
 run `./server.py`
 
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from flask import Flask, request
+from cachetools.func import ttl_cache
+import subprocess
 import click
 
 
@@ -7,25 +9,37 @@ DEFAULT_HOST='0.0.0.0'
 DEFAULT_PORT=8080
 app = Flask(__name__)
 
+
 @click.command()
 @click.option('--host', default=DEFAULT_HOST, help='What host to broadcast')
 @click.option('--port', default=DEFAULT_PORT, help='What port to bind to')
-def run_server(host: str, port: int):
+def run_server(host: str, port: int) -> None:
     """easy web interface to control gate and garage doors"""
     app.run(host=host, port=port)
 
 
 @app.route("/")
 @app.route("/index")
-def index():
+def index() -> str:
+    """ return the contents of index.html in the response body """
     with open('index.html', 'r') as file:
         return file.read()
 
 
+@ttl_cache(ttl=5) # memoize for 5 seconds to prevent spamming
+def ding() -> str:
+    """ play a sound when someone buzzes in """
+    # sudo apt-get install cvlc
+    cmd = "cvlc --play-and-exit ding.mp3"
+    subprocess.Popen(cmd, shell=True) # non-blocking
+
+
 @app.route("/buzz", methods = ['POST'])
-def buzz():
+def buzz() -> str:
+    """ someone just clicked the buzzer """
     click.echo(request.form.get('player'))
-    return "BUZZ!"
+    ding()
+    return "BUZZ!!"
 
 
 if __name__ == '__main__':
