491cdf6686e9d1b51823a8752fbfbd3dd18154d6
Scott Leland Crossen <scottcrossen42@gmail.com>
2018-09-29 17:45:48 -0700

    scoreboard

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,37 @@
 import {MDCRipple} from '@material/ripple'
 import {MDCTextField} from '@material/textfield'
 
-const username = new MDCTextField(document.querySelector('.username'))
-new MDCRipple(document.querySelector('.next'))
-
-document.querySelector('.login').addEventListener('submit', () => {
-  document.querySelector('.page-1').style.display = 'none';
-  document.querySelector('.page-2').style.display = 'flex';
-})
-
-document.querySelector('.buzz').addEventListener('click', () => {
-  const xmlHttp = new XMLHttpRequest();
-  xmlHttp.open('POST', `/buzz?player=${username.value}`, true);
-  xmlHttp.send(username.value);
-})
+if (window.location.pathname == '/') {
+  const username = new MDCTextField(document.querySelector('.username'))
+  new MDCRipple(document.querySelector('.next'))
+  document.querySelector('.login').addEventListener('submit', () => {
+    document.querySelector('.page-1').style.display = 'none'
+    document.querySelector('.page-2').style.display = 'flex'
+  })
+  document.querySelector('.buzz').addEventListener('click', () => {
+    const xmlHttp = new XMLHttpRequest()
+    xmlHttp.open('POST', `/buzz?player=${username.value}`, true)
+    xmlHttp.send(username.value)
+  })
+} else if (window.location.pathname == '/scoreboard.html') {
+  const xmlHttp = new XMLHttpRequest()
+  xmlHttp.open('GET', '/score', true)
+  xmlHttp.responseType = 'json'
+  var list = document.querySelector('.scoreboard')
+  xmlHttp.onload = function() {
+    Object.keys(this.response).forEach((key) => {
+      var newListElement = document.createElement('li')
+      newListElement.classList.add('mdc-list-item')
+      var newLeftSpan = document.createElement('h3')
+      newLeftSpan.classList.add('mdc-list-item__text')
+      newLeftSpan.textContent = key
+      var newRightSpan = document.createElement('h3')
+      newRightSpan.classList.add('mdc-list-item__text')
+      newRightSpan.textContent = this.response[key]
+      newListElement.appendChild(newLeftSpan)
+      newListElement.appendChild(newRightSpan)
+      list.appendChild(newListElement)
+    })
+  }
+  xmlHttp.send()
+}
